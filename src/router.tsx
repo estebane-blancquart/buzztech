7da00848d2c49b7b50b0a0b516aafce6e9bde194
estebane-blancquart <estebane.blancquart@gmail.com>
2024-07-12 02:16:32 +0200

    [func] - router

---
+++ src/router.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+function AppRouter() {
+  return (
+    <Router>
+      <Routes>
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
