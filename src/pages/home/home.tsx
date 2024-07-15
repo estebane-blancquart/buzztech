aec9aa0b05b18e8ab009a10f66f4036a109ed5d1
estebane-blancquart <estebane.blancquart@gmail.com>
2024-07-15 18:33:20 +0200

    [func] - add contact component to home page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import styles from './home.module.scss';
 import Header from '../../components/header/header';
 import Navigation from '../../components/navigation/navigation';
+import Contact from '../../components/contact/contact';
 
 function Home() {
   return (
     <div className={styles.home}>
       <Header />
       <Navigation />
+      <Contact />
     </div>
   )
 }
