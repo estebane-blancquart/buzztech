3f5491095d3304157d7de266c1e7f6c499e49a8a
estebane-blancquart <estebane.blancquart@gmail.com>
2024-07-18 17:40:36 +0200

    [func] - service component

---
+++ src/components/service/service.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './service.module.scss';
+
+interface ServiceProps {
+    id: string;
+    title: string;
+    description: string;
+    price: string;
+}
+
+export default function Service({ id, title, description, price }: ServiceProps) {
+    return (
+        <>
+            <section id={id}>
+                <h2>{title}</h2>
+                <p>{description}</p>
+                <p><span className='price'>Tarif:</span>{price}</p>
+            </section>
+        </>
+    );
+}
