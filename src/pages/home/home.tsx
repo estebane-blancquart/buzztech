efcc5be32e267e58d20cf049da16db4f117ee6a7
estebane-blancquart <estebane.blancquart@gmail.com>
2024-07-18 17:33:39 +0200

    [func] - add survey to home page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -10,6 +10,13 @@ function Home() {
       <Header />
       <Navigation />
       <Contact />
+
+      <div className={styles.survey}>
+        <h2>Obtenez une estimation gratuite et rapide.</h2>
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLSdjyQ4WdzbyPgZ2iGqKjwTObb3-Lt7Ns1pDEKv0DOfjQacK8Q/viewform?usp=sf_link">Demandez un devis</a>
+      </div>
+
+      <img className={styles.sphere} src="images/sphere.png" alt="" />
     </div>
   )
 }
