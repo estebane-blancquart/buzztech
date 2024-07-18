7064ff5f14af41b158c6a2b699459a368db8d921
estebane-blancquart <estebane.blancquart@gmail.com>
2024-07-18 17:38:02 +0200

    [func] - package component

---
+++ src/components/package/package.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './package.module.scss';
+
+interface PackageProps {
+    id: string;
+    title: string;
+    summary: string;
+    description: string;
+    price: string;
+}
+
+export default function Package({ id, title, summary, description, price }: PackageProps) {
+    return (
+        <>
+            <section id={id}>
+                <h2>{title}</h2>
+                <p>{summary}</p>
+                <p>{description}</p>
+                <p><span className='price'>Tarif:</span>{price}</p>
+                <button>Demander un devis.</button>
+            </section>
+        </>
+    );
+}
