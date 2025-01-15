93c59d8034d502cb41a83099f1960b1738144499
estebane-blancquart <estebane.blancquart@gmail.com>
2025-01-15 16:17:57 +0100

    [func] - transitions

--- src/router.tsx
+++ src/router.tsx
@@ -1,21 +1,39 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import Home from './pages/home/home';
 import Cleaning from './pages/presentation/cleaning';
 import Configuration from './pages/presentation/configuration';
 import Development from './pages/presentation/development';
 import Repair from './pages/presentation/repair';
+import './styles/transitions.scss';
+
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  return (
+    <TransitionGroup>
+      <CSSTransition
+        key={location.key}
+        classNames={location.pathname === '/' ? '' : 'fade'}
+        timeout={location.pathname === '/' ? 0 : 500}
+      >
+        <Routes location={location}>
+          <Route path="/" element={<Home />} />
+          <Route path="/nettoyage" element={<Cleaning />} />
+          <Route path="/conception" element={<Configuration />} />
+          <Route path="/developpement" element={<Development />} />
+          <Route path="/depannage" element={<Repair />} />
+        </Routes>
+      </CSSTransition>
+    </TransitionGroup>
+  );
+}
 
 function AppRouter() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/nettoyage" element={<Cleaning />} />
-        <Route path="/conception" element={<Configuration />} />
-        <Route path="/developpement" element={<Development />} />
-        <Route path="/depannage" element={<Repair />} />
-      </Routes>
+      <AnimatedRoutes />
     </Router>
   );
 }
