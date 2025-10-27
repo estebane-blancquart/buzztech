c122c752695473dc53101b16a5c78826b8f8d5db
estebane-blancquart <estebane.blancquart.outils@proton.me>
2025-10-27 12:21:28 +0100

    sdfsfsdf

--- src/ui/components/header/Header.tsx
+++ src/ui/components/header/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './Header.module.scss';
+import styles from './header.module.scss';
 import ScrollLink from '../scroller/scrollLink';
 import { useToggle } from '@/core/hooks/useToggle';
 
