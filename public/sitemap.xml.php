0a2b4b63fd7ee741c944c89aa4b9d513f600b309
estebane-blancquart <estebane.blancquart.outils@proton.me>
2025-10-28 22:29:35 +0100

    wxcwcwxcwxc

--- public/sitemap.xml.php
+++ public/sitemap.xml.php
@@ -1,4 +1,7 @@
-<?xml version="1.0" encoding="UTF-8"?>
+<?php
+header('Content-Type: application/xml; charset=UTF-8');
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <!-- Page d'accueil -->
   <url>
